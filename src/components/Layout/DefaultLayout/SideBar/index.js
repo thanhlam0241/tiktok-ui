52e8b2dbd8c8b0c864fa548030af4f50cbb1a4af
Genshin <thanhlam02412002@gmail.com>
2022-12-05 23:30:58 +0700

    hello

---
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -0,0 +1,7 @@
+function SideBar() {
+    return (
+        <h1>Side Bar</h1>
+    );
+}
+
+export default SideBar;
---
+++ src/pages/Following/index.js
@@ -0,0 +1,7 @@
+function Following() {
+    return (
+        <h2>Folowing page</h2>
+    );
+}
+
+export default Following;
