419075ba972d357037ba9dc667f0084609912d14
Genshin <thanhlam02412002@gmail.com>
2022-12-09 14:27:30 +0700

    adfd

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,6 +1,14 @@
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
+
+
+const cx = classNames.bind(styles);
+
 function SideBar() {
     return (
-        <h1>Side Bar</h1>
+        <aside className={cx('wrapper')}>
+            <h2>Sidebar</h2>
+        </aside>
     );
 }
 
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,33 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+import images from '../../../../asset/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'; //alt + mui ten dich chuyen
+const cx = classNames.bind(styles);
+// spin = { true}
 function Header() {
     return (
-        <h2>Header</h2>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt='logo' />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder='Search acounts and videos' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                </div>
+            </div>
+        </header>
     );
 }
 
